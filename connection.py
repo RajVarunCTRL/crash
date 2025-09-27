311ea233dae3db0ba66dbaab93829de5e96d25e5
RajVarun <grajvarun@gmail.com>
2025-09-27 22:14:59 +0530

    1. Connecting to a mongo cloud db, returning sample dbs

---
+++ connection.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+from config import MONGODB_URI
+
+from pymongo import MongoClient
+
+client = MongoClient(MONGODB_URI)
+
+for db_name in client.list_database_names():
+    print(db_name)
